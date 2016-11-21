065d79d6c75be0f47ee834091844a522c5f0ac18
barischj <barischj@tcd.ie>
2016-11-21 20:48:38 +0000

    improved readability

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # expired_cache
+
+A cache which returns an expired value while the value is being updated.
+
+The cached value of `fn(a, k)` is only updated when that call is made and the
+cached value has existed for `ttl` seconds, not as soon as the cached value
+expires.
+
+The cached value is updated in another thread. The cache doesn't wait for the
+fresh value to be computed but instead returns the expired value.
+
+This means that while a call might return an expired value, as many calls as
+possible will return without waiting for the cached value to be updated. The
+exception is when the cached value doesn't exist yet. Ensure the cached value
+is computed as soon as possible make a call when the module is imported.
--- expired_cache.py
+++ expired_cache.py
@@ -1,14 +1,3 @@
-"""A cache with lazy background updating of data.
-
-The cached value of a function call `fn(a, k)` is updated when that call is made
-and the cached value has existed for `ttl` seconds. The cached value is updated
-in another thread and the expired cached value returned. This means that while a
-call can return an expired cached value, as many calls as possible will return
-immediately. The exceptions are the calls made before the initial cached value
-is computed. To ensure the cached value is computed as soon as possible make a
-call as soon as the module is imported.
-"""
-
 import pylru
 import threading
 import time
@@ -17,39 +6,31 @@ import time
 def Cache(object):
 
     def __init__(self, ttl, max_size):
-        self.ttl = ttl  # seconds
-        self.cache = pylru.lrucache(max_size)  # {key: (expires, value)}
-        lock = threading.Lock()
+        self._ttl = ttl  # seconds
+        self._cache = pylru.lrucache(max_size)  # {key: (expires, value)}
+        self._lock = threading.Lock()
 
     def _update(self, key, fn, args, kwargs):
-        """Returns the newly set cached value."""
-        value = fn(*args, **kwargs)
-        self.cache[key] = (time.time() + self.ttl, value)
-        return value
+        """Updates a cached value."""
+        fresh_value = fn(*args, **kwargs)
+        self._cache[key] = (time.time() + self._ttl, fresh_value)
 
     def get(self, fn, args, kwargs):
-        """Returns the currently cached copy."""
+        """Returns a cached value."""
         key = (args, kwargs)
-        (expires, value) = lrucache
-
-
-
-        key = (args, kwargs)
-        # If there exists a cached value.
         if key in self.cache:
-            (expires, value) = lru_cache[key]
-            # Update cached value if expired and not already being updated...
-            if expires > time.time() and update_lock.acquire(blocking=False):
+            (expires, value) = self._cache[key]
+            # If cached value is expired and not already being updated
+            if expires > time.time() and self._lock.acquire(blocking=False):
                 def update_and_release():
                     try:
                         self._update(key, fn, args, kwargs)
                     finally:
-                        update_lock.release()
+                        self._lock.release()
                 threading.Thread(target=update_and_release).start()
-            # ...but still return expired copy.
             return value
-        # Else cache and return a new value.
-        return self._update(key, fn, args, kwargs)
+        self._update(key, fn, args, kwargs)
+        return self._cache[key]
 
 
 def cache(*cache_args):
